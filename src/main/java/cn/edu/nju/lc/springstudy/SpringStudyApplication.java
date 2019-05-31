ff9dbe6cd01bfaca1f4a9ef6debd58cfce454886
chengliu <1397004810@qq.com>
2019-05-31 22:22:41 +0800

    first commit

---
+++ src/main/java/cn/edu/nju/lc/springstudy/SpringStudyApplication.java
@@ -0,0 +1,13 @@
+package cn.edu.nju.lc.springstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringStudyApplication.class, args);
+	}
+
+}
---
+++ src/main/java/cn/edu/nju/lc/springstudy/properties/Person.java
@@ -0,0 +1,4 @@
+package cn.edu.nju.lc.springstudy.properties;
+
+public class Person {
+}
