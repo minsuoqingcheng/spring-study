f17a07cd56d11f43184ebd4bba57026e4121fad7
chengliu <1397004810@qq.com>
2019-05-31 23:19:59 +0800

    [Test1] 在resource文件夹下配置代码中的属性，使用单独的文件，不放置在application.yml下，但是文件需要以.properties结尾

--- src/main/java/cn/edu/nju/lc/springstudy/SpringStudyApplication.java
+++ src/main/java/cn/edu/nju/lc/springstudy/SpringStudyApplication.java
@@ -1,13 +1,17 @@
 package cn.edu.nju.lc.springstudy;
 
+import cn.edu.nju.lc.springstudy.properties.Person;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class SpringStudyApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringStudyApplication.class, args);
+		ConfigurableApplicationContext context = SpringApplication.run(SpringStudyApplication.class, args);
+		Person person = context.getBean(Person.class);
+		System.out.println(person.toString());
 	}
 
 }
--- src/main/java/cn/edu/nju/lc/springstudy/properties/Person.java
+++ src/main/java/cn/edu/nju/lc/springstudy/properties/Person.java
@@ -1,4 +1,23 @@
 package cn.edu.nju.lc.springstudy.properties;
 
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Component;
+
+@Component
+@PropertySource(value = {"classpath:person.properties"})
+@ConfigurationProperties(prefix = "person")
+@Getter
+@Setter
+@ToString
 public class Person {
+
+    private Long id;
+    private String name;
+    private Integer age;
+
 }
