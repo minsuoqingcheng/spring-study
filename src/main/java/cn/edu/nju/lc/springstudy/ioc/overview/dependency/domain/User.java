5c5275280a152528622f93cab000723d1872e18e
liucheng <liucheng@superid.cn>
2020-08-06 23:03:16 +0800

    基础类型的集合注入

--- src/main/java/cn/edu/nju/lc/springstudy/ioc/overview/dependency/domain/User.java
+++ src/main/java/cn/edu/nju/lc/springstudy/ioc/overview/dependency/domain/User.java
@@ -1,5 +1,6 @@
 package cn.edu.nju.lc.springstudy.ioc.overview.dependency.domain;
 
+import cn.edu.nju.lc.springstudy.ioc.overview.dependency.enums.City;
 import lombok.Data;
 
 @Data
@@ -8,6 +9,8 @@ public class User {
     private Long id;
 
     private String name;
+
+    private City[] cities;
 
     public static User createUser() {
         return new User();
---
+++ src/main/java/cn/edu/nju/lc/springstudy/ioc/overview/dependency/enums/City.java
@@ -0,0 +1,8 @@
+package cn.edu.nju.lc.springstudy.ioc.overview.dependency.enums;
+
+public enum City {
+
+    SHANGHAI,
+    NANJING
+
+}
